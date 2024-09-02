d73233472f26da7918c606bc670e3e6599b56cc7
adriendemange <adrien.demange@hecten.fr>
2024-09-02 14:24:38 +0200

    Add files via upload

---
+++ B1_RESEAU/fonction_1.py
@@ -0,0 +1,49 @@
+#Adrien DEMANGE
+#2 septembre 2024
+#fonction_1 python 
+
+
+
+
+def my_function():
+  i=0
+  while i<5 :
+    print("Coucou")
+    i=i+1
+#fin
+    
+my_function()
+
+def bonjour(fname):
+    a=fname
+    print("Bonjour", a)
+
+    
+bonjour("Adrien")
+#fin
+def carre():
+   a=23*23
+   print("Le carre de 23 est", a)
+#fin
+carre()
+def somme():
+   i=8
+   a=0
+   while i<20 : 
+    a = a+i
+    i=i+1
+    print("La somme des nombres de 8 à 20 :", a)
+#fin
+somme()
+def puissance(x, y):
+   a=0
+   i=0
+   while i < y :
+        a = x*x
+        i = i+1
+print("Valeur de a :", a)
+
+puissance(5, 2)
+
+   
+
