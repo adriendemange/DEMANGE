a3ddde6b8285c3a04a478114fd677c368dbed5ac
adriendemange <adrien.demange@hecten.fr>
2025-02-21 17:04:32 +0100

    Easyline2Completé

--- JAVA/Projet_IntelliJ/Easyline/src/fr/hecten/Easyline2.java
+++ JAVA/Projet_IntelliJ/Easyline/src/fr/hecten/Easyline2.java
@@ -7,22 +7,50 @@ import java.util.Scanner;
 
 public class Easyline2{
     public static void main(String[] args){
-        Compagnie maCompagnie1= new Compagnie();
-        maCompagnie1.nom="zaza";
-        maCompagnie1.code=10;
-        maCompagnie1.couleurPrincipaleLogo = "Bleue";
-        System.out.println("Compagne 1 - Code - Couleur : " + maCompagnie1.code + " " + maCompagnie1.couleurPrincipaleLogo);
+
+        Compagnie maCompagnie1= new Compagnie("zozo", "Bleu", "Vert", 25);
+        //maCompagnie1.code=10;
         maCompagnie1.afficher();
-        Compagnie maCompagnie2= new Compagnie("zozo");
-        maCompagnie2.code=20;
-        System.out.println("Compagne 1 - Code : " + maCompagnie2.code);
-        Compagnie maCompagnie3= new Compagnie("easyJune");
+
+        System.out.println("-------------------------");
+        Compagnie maCompagnie2= new Compagnie("zozo", "Bleu", "Cyan", 60);
+        //maCompagnie2.code=20;
+        maCompagnie2.afficher();
+
+        System.out.println("-------------------------");
+        Compagnie maCompagnie3= new Compagnie("easyJune", "Rouge", "Gold", 25);
+        //maCompagnie3.code=20;
         maCompagnie3.afficher();
 
-        Compagnie maCompagnie4= new Compagnie();
+        System.out.println("-------------------------");
+        Compagnie maCompagnie4= new Compagnie("Gon", "Platine", "Bronze", 20);
+        //maCompagnie4.code=20;
         maCompagnie4.afficher();
-        Compagnie maCompagnie5= new Compagnie();
+
+        System.out.println("-------------------------");
+        Compagnie maCompagnie5= new Compagnie("toto", "Noir", "Blanc", 21);
+        //maCompagnie5.code=20;
         maCompagnie5.afficher();
+
+        System.out.println("-------------------------");
+        Compagnie maCompagnie7= new Compagnie("toto_2", "Noir", "Blanc", 22);
+        //maCompagnie5.code=20;
+        maCompagnie7.afficher();
+       /* int MDP = maCompagnie7.getCode();
+        if (MDP ==  maCompagnie7.code) {
+            System.out.println("Voici le MDP de la compagnie " + maCompagnie7.nom + " : " + MDP);
+        }
+        */
+
+        String CouleurPrinc7 = maCompagnie7.getCouleurSecondaireLogo();
+        String CouleurSec7 = maCompagnie7.getCouleurPrincipaleLogo();
+        System.out.println(CouleurPrinc7 + " " + CouleurSec7);
+        maCompagnie7.setCode();
+        int MDP = maCompagnie7.getCode();
+        if (MDP ==  maCompagnie7.code) {
+            System.out.println("Voici le MDP de la compagnie " + maCompagnie7.nom + " : " + MDP);
+        }
+
 
     }
 }
